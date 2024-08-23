224a0510d3afdbe75ba129efcb1f81c630432a42
Abdulaziz200408 <oddiy5267@gmail.com>
2024-08-23 12:24:01 +0500

    DS

--- src/components/page/buyurtmalar/buyurtmalar.tsx
+++ src/components/page/buyurtmalar/buyurtmalar.tsx
@@ -1,13 +1,34 @@
-import { Button, Input } from "antd";
-import React, { useState } from "react";
-import { FiPlus } from "react-icons/fi";
+import { Button, Card, Col, Input, Row } from "antd";
+import React, { useEffect, useState } from "react";
+import { FiPlus, FiTrash2 } from "react-icons/fi";
 import { IoSearchOutline } from "react-icons/io5";
 import "../buyurtmalar/css.css";
 import { HiMenuAlt4 } from "react-icons/hi";
 import { HiOutlineMenuAlt3 } from "react-icons/hi";
+import axios from "axios";
+import { LuPen } from "react-icons/lu";
+interface Product {
+  id: number;
+  nameuz: string;
+  nameru: string;
+  locate: string;
+  hour: string;
+}
 
 function Buyurtmalar() {
   const [activeButton, setActiveButton] = useState("Yangi");
+  const [data, setData] = useState<Product[]>([]);
+
+  useEffect(() => {
+    axios
+      .get("https://4a39859802af3eec.mokky.dev/filiallar")
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }, []);
 
   const handleButtonClick = (buttonName: string) => {
     setActiveButton(buttonName);
@@ -23,7 +44,8 @@ function Buyurtmalar() {
             paddingLeft: "50px",
             width: "270px",
           }}
-          className="flex items-center gap-5">
+          className="flex items-center gap-5"
+        >
           <Button
             style={{
               borderRadius: "50%",
@@ -38,7 +60,8 @@ function Buyurtmalar() {
           <h2
             style={{
               fontWeight: "bold",
-            }}>
+            }}
+          >
             Yangi filial
             <br />
             qo’shish
@@ -55,25 +78,30 @@ function Buyurtmalar() {
             borderRadius: "30px",
             padding: "5px",
             marginLeft: "50px",
-          }}>
+          }}
+        >
           <button
             className={activeButton === "Yangi" ? "activ" : "inActiv"}
-            onClick={() => handleButtonClick("Yangi")}>
+            onClick={() => handleButtonClick("Yangi")}
+          >
             Yangi
           </button>
           <button
             className={activeButton === "Qabul qilingan" ? "activ" : "inActiv"}
-            onClick={() => handleButtonClick("Qabul qilingan")}>
+            onClick={() => handleButtonClick("Qabul qilingan")}
+          >
             Qabul qilingan
           </button>
           <button
             className={activeButton === "Jo’natilgan" ? "activ" : "inActiv"}
-            onClick={() => handleButtonClick("Jo’natilgan")}>
+            onClick={() => handleButtonClick("Jo’natilgan")}
+          >
             Jo’natilgan
           </button>
           <button
             className={activeButton === "Yopilgan" ? "activ" : "inActiv"}
-            onClick={() => handleButtonClick("Yopilgan")}>
+            onClick={() => handleButtonClick("Yopilgan")}
+          >
             Yopilgan
           </button>
         </div>
@@ -86,7 +114,8 @@ function Buyurtmalar() {
             width: "100px",
             marginLeft: "290px",
           }}
-          className="flex items-center gap-5">
+          className="flex items-center gap-5"
+        >
           <div
             className=" flex items-center justify-between"
             style={{
@@ -97,7 +126,8 @@ function Buyurtmalar() {
               borderRadius: "30px",
               padding: "7px",
               marginLeft: "20px",
-            }}>
+            }}
+          >
             <Button className="iconActiv">
               <HiMenuAlt4 />
             </Button>
@@ -107,6 +137,81 @@ function Buyurtmalar() {
           </div>
         </div>
       </div>
+
+      <Row>
+        <div className=" mt-10 w-full">
+          {data.map((f) => (
+            <Col
+              span={24}
+              style={{ padding: "13px", marginTop: -14 }}
+              key={f.id}
+            >
+              <Card
+                style={{
+                  borderRadius: "8px",
+                  boxShadow: "1px 1px 10px rgba(124, 124, 124, 0.3)",
+                  height: "180px",
+                }}
+                hoverable
+              >
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "space-around",
+                    alignItems: "center",
+                    textAlign: "start",
+                  }}
+                >
+                  <div
+                    style={{
+                      display: "flex",
+                      gap: "30px",
+                      alignItems: "center",
+                      width: `calc(100% / 5)`,
+                    }}
+                  >
+                    <p>{f.nameuz}</p>
+                  </div>
+                  <div style={{ width: `calc(100% / 5)` }}>
+                    <p>{f.nameru}</p>
+                  </div>
+                  <div style={{ width: `calc(100% / 5)` }}>
+                    <p>{f.locate}</p>
+                  </div>
+                  <div style={{ width: `calc(100% / 5)` }}>
+                    <p>{f.hour}</p>
+                  </div>
+
+                  <div className="flex space-x-2 mt-2">
+                    <Button
+                      style={{
+                        borderRadius: "50%",
+                        width: "30px",
+                        height: "30px",
+                        boxShadow: "0px 2px 2px 0px #AEB0B550",
+                      }}
+                      className="bg-white flex items-center justify-center transition-transform duration-300 hover:scale-110"
+                    >
+                      <LuPen />
+                    </Button>
+                    <Button
+                      style={{
+                        borderRadius: "50%",
+                        width: "30px",
+                        height: "30px",
+                        boxShadow: "0px 2px 2px 0px #AEB0B550",
+                      }}
+                      className="bg-white flex items-center justify-center transition-transform duration-300 hover:scale-110"
+                    >
+                      <FiTrash2 />
+                    </Button>
+                  </div>
+                </div>
+              </Card>
+            </Col>
+          ))}
+        </div>
+      </Row>
     </div>
   );
 }
