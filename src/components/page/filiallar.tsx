2008ee688d68bed9b0f123ebf0f352e6e050b2ef
Abdulaziz200408 <oddiy5267@gmail.com>
2024-08-23 06:58:50 +0500

    cd

--- src/components/page/filiallar.tsx
+++ src/components/page/filiallar.tsx
@@ -1,11 +1,9 @@
-import { Button, Form, Input, Modal } from "antd";
-import React, { useState } from "react";
+import { Button, Input } from "antd";
+import React from "react";
 import { FiPlus } from "react-icons/fi";
-import { IoSearchOutline } from "react-icons/io5"; // Import qilish
+import { IoSearchOutline } from "react-icons/io5";
 
 const Filiallar: React.FC = () => {
-
-
   return (
     <div>
       <div className="bg-white flex items-center">
@@ -70,7 +68,40 @@ const Filiallar: React.FC = () => {
         </div>
       </div>
 
-    
+      {/* Table */}
+      <div className="bg-white p-4 flex justify-around items-center mt-6" style={{ width: "100%", boxShadow: "0px 3px 6px 0px #8D9BA85C", fontWeight: "bold" }}>
+        <h3>Filial nomi (UZ)</h3>
+        <h3>Filial nomi (Ru)</h3>
+        <h3>Mo’ljal</h3>
+        <h3>Ish vaqti</h3>
+        <h3>ACTION</h3>
+      </div>
+
+      {Array.from({ length: 7 }).map((_, index) => (
+        <div
+          key={index}
+          style={{
+            width: "1266px",
+            height: "46px",
+            marginLeft: "40px",
+            borderRadius: "10px",
+            boxShadow: "0px 2px 2px 0px #AEB0B550",
+            padding: "10px 0px 0px 10px",
+            transition: "transform 0.2s ease, box-shadow 0.2s ease",
+          }}
+          className="mt-5 bg-white w-full flex justify-around items-center hover:transform hover:translate-y-[-5px] hover:shadow-lg"
+        >
+          <h2>Shaxrishton</h2>
+          <h2>Shaxrishton</h2>
+          <h2>Metro ro’parasida</h2>
+          <h2>09:00 - 20:00</h2>
+          <div className="flex justify-center gap-4">
+            <button>locate</button>
+            <button>edit</button>
+            <button>delete</button>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
